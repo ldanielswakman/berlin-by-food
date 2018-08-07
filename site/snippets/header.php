cda8f431a91fde54b6376caea628f84d91943418
L Daniel Swakman <d.swakman@gmail.com>
2018-08-07 17:08:31 +0200

    added site meta

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,11 +4,9 @@
 
   <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title><?= $page->title()->html() ?> | <?= $site->title()->html() ?></title>
 
-    <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
-    <meta name="description" content="<?= $site->description()->html() ?>">
+    <? snippet('head-meta') ?>
 
     <? snippet('google-analytics') ?>
 
