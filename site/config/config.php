3c50c3e206e6aa99b985066521a66fc9392c4dde
L Daniel Swakman <d.swakman@gmail.com>
2018-08-07 14:19:13 +0200

    ignore cache for certain pages

--- site/config/config.php
+++ site/config/config.php
@@ -3,23 +3,6 @@
 /*
 
 ---------------------------------------
-License Setup
----------------------------------------
-
-Please add your license key, which you've received
-via email after purchasing Kirby on http://getkirby.com/buy
-
-It is not permitted to run a public website without a
-valid license key. Please read the End User License Agreement
-for more information: http://getkirby.com/license
-
-*/
-
-c::set('license', 'put your license key here');
-
-/*
-
----------------------------------------
 Kirby Configuration
 ---------------------------------------
 
@@ -29,6 +12,13 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
 
+// Ignore cache for pages
+c::set('cache.ignore', [
+  'contact',
+  'tours/*',
+]);
+
+// Define Languages
 c::set('languages', array(
   array(
     'code'    => 'en',
@@ -51,7 +41,7 @@ c::set('languages', array(
   ),
 ));
 
-
+// Define Kirby panel widgets
 c::set('panel.widgets', array(
   'account'  => true,
   'history'  => true,
