8a363520c0d07370edf4a785e0db291379547efb
L Daniel Swakman <hello@ldaniel.eu>
2018-08-12 13:25:13 +0000

    content update

--- site/languages/en.php
+++ site/languages/en.php
@@ -1,9 +1,9 @@
 <?php 
 
-l::set('read_more', 'En garde');
+l::set('read_more', 'Read more');
 l::set('interested', 'I\'m interested');
 l::set('more_info', 'More info');
-l::set('request_tour', 'Request tour');
+l::set('request_tour', 'Take the tour');
 l::set('see_all', 'See all');
 l::set('read_blog', 'Read our blog');
 l::set('duration', 'Duration');
@@ -18,4 +18,4 @@ l::set('message', 'Message');
 l::set('send', 'Send');
 l::set('error_email', 'Please enter a valid email address');
 l::set('error_message', 'Please enter a message (min 10 characters)');
-l::set('success_message', 'Thank you for your inquiry. We will get back to you soon!');
+l::set('success_message', 'Thank you for your enquiry. We\'ll get back to you soon!');
