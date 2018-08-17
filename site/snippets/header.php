907493ce79bdffcbf9efc73b8f3bcdf4172a17a0
L Daniel Swakman <d.swakman@gmail.com>
2018-08-17 10:59:04 +0200

    re-added coming soon if not logged in

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <?
 // Temporary redirect; remove when going live
-// if(!$site->user() && page()->template() != 'soon') go('soon');
+if(!$site->user() && page()->template() != 'soon') go('soon');
 ?>
 
 <html lang="<?= site()->language() ? site()->language()->code() : 'en' ?>">
