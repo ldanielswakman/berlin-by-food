f05e8085ebf9399f1137bc9f8102c89c050ac951
L Daniel Swakman <d.swakman@gmail.com>
2018-10-12 11:20:49 +0200

    tweaked intro loading

--- site/templates/home.php
+++ site/templates/home.php
@@ -6,7 +6,6 @@
   
   <section id="intro" class="intro">
     <?= $page->intro()->kirbytext() ?>
-    <a href="#more" class="home-more"><img src="<?= url('assets/images/arrow-down.svg') ?>" alt="" /></a>
   </section>
 
   <section id="more" class="more">
