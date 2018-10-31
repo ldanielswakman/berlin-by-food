e3c1471976abf8b75b703cfec80c9c8510877964
L Daniel Swakman <d.swakman@gmail.com>
2018-10-31 15:12:13 +0100

    proper email addresses to form submissions

--- site/controllers/tour.php
+++ site/controllers/tour.php
@@ -1,44 +1,9 @@
 <?
 
-use Uniform\Form;
+return function ($site, $pages, $page) {
 
-return function ($site, $pages, $page)
-{
-    $form = new Form([
-        'email' => [
-            'rules' => ['required', 'email'],
-            'message' => l::get('error_email'),
-        ],
-        'name' => [],
-        'message' => [
-            'rules' => ['required', 'minLength' => 10],
-            'message' => l::get('error_message'),
-        ],
-    ]);
-
-    $prefix = '[Berlin by Food] New message';
-    $subject = ($form->data('name')) ? $prefix . ' from ' . $form->data('name') : $prefix;
-
-    if (r::is('POST')) {
-
-        $form->logAction([
-            'file' => kirby()->roots()->site() . '/form-submissions.log',
-        ]);
-
-        // Send email if not in dev env
-        if (c::get('env') !== 'DEV') :
-            $form->emailAction([
-                'to' => 'info@berlinbyfood.eu',
-                'from' => $form->data('email'),
-                'subject' => $subject,
-            ])
-            ->emailAction([
-                'to' => 'daniel@berlinbyfood.eu',
-                'from' => $form->data('email'),
-                'subject' => $subject,
-            ]);
-        endif;
-    }
+    require_once(kirby()->roots()->controllers() . '/shared/contact-form.php');
 
     return compact('form');
+    
 };
