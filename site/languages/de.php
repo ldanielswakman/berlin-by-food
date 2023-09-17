c81164398acf09eec31869bdfb6dd9ae2c110a10
L Daniel Swakman <hello@ldaniel.eu>
2023-09-17 21:54:27 +0000

    content update

--- site/languages/de.php
+++ site/languages/de.php
@@ -22,19 +22,14 @@ l::set('error_message', 'Bitte eine Nachricht eingeben (mindestens 10 Zeichen)')
 l::set('success_message', 'Vielen Dank für Deine Anfrage. Wir melden uns bald!');
 l::set('article_appeared_in', 'Dieser Artikel erschien ursprünglich auf');
 l::set('see_original', 'Hier lesen');
-
 l::set('cookie_message', 'Diese Website verwendet Cookies, um sicherzustellen, dass Du die beste Erfahrung auf unserer Website hast..');
 l::set('cookie_learn_more', 'Mehr erfahren');
 l::set('cookie_button_text', 'Cookies zulassen');
-
 l::set('form_data_consent_text', 'In diesem Formular werden Deine E-Mail-Adresse und Name erfasst, damit wir bezüglich Deiner Anfrage kontaktieren können. In unserem Impressum siehst Du, wie wir Deine eingereichten Daten schützen und verwalten.');
 l::set('form_data_consent_agree', 'Ich stimme zu, dass mein Name und meine E-Mail Adresse von Berlin by Food gespeichert werden.');
 l::set('form_data_consent_error', 'Bitte dieses Kästchen ankreuzen, um fortzufahren');
-
 l::set('sticker', 'Die Touren fangen nach der COVID-19-Krise an');
-
 l::set('newsletter-email-placeholder', 'your@email.address');
 l::set('newsletter-signup-title', 'Do you like Berlin by Food? ');
 l::set('newsletter-signup-description', 'Sign up to be informed of future blog posts and other news.');
-
-l::set('related_title', 'Next articles to read');
+l::set('related_title', 'Other posts that could interest you');
--- site/languages/en.php
+++ site/languages/en.php
@@ -6,7 +6,7 @@ l::set('interested', 'I\'m in');
 l::set('more_info', 'Find out more');
 l::set('request_tour', 'Book a tour');
 l::set('see_all', 'See all tours');
-l::set('read_blog', 'Read the blog');
+l::set('read_blog', 'To the blog');
 l::set('duration', 'Duration');
 l::set('hour', 'hour');
 l::set('hours', 'hours');
@@ -22,19 +22,14 @@ l::set('error_message', 'Please enter a message (min 10 characters)');
 l::set('success_message', 'Thank you for your enquiry. We\'ll get back to you ASAP!');
 l::set('article_appeared_in', 'This article originally appeared on');
 l::set('see_original', 'Read it here');
-
 l::set('cookie_message', 'This website uses cookies to ensure you get the best experience on our website.');
 l::set('cookie_learn_more', 'Learn more');
 l::set('cookie_button_text', 'Allow cookies');
-
 l::set('form_data_consent_text', 'This form collects your email and name so that we can get in touch with you about your request. Read our Impressum for the full story on how we protect and manage your submitted data.');
 l::set('form_data_consent_agree', 'I consent to Berlin by Food collecting my email and name');
 l::set('form_data_consent_error', 'Please tick this box to continue');
-
 l::set('sticker', 'Tours begin after the pandemic');
-
 l::set('newsletter-email-placeholder', 'your@email.address');
 l::set('newsletter-signup-title', 'Do you like Berlin by Food? ');
 l::set('newsletter-signup-description', 'Sign up to be informed of future blog posts and other news.');
-
-l::set('related_title', 'Next articles to read');
+l::set('related_title', 'Other posts that could interest you');
